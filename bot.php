9b7c62e713cd83d1a100466765527da627ce50f0
Bestyiix <32898297+Bestyiix@users.noreply.github.com>
2017-10-18 22:39:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,14 +1,9 @@
 <?php
 $access_token = 'NAekpw2PvpewrFKX38GXYA4soepOM+wNzujSvvh3QDiiDyGEm8RnU+gytDh4CuYDLAH4G7E1I+lzANS+UB+Y4P2uEnZyJW/GW2F35hjIDGlS9KnMxlxEFD0TEGbB30xorS+GijIOadGhWbODQlz+rAdB04t89/1O/w1cDnyilFU=';
-$_SESSION["data"] = "green";
-// Get Chat 
-$Chat = "Unknow";
 // Get POST body content
 $content = file_get_contents('php://input');
-$Chat = $content;
 // Parse JSON
 $events = json_decode($content, true);
-$Chat = $events;
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -17,11 +12,10 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$Chat = $text;
-			$_SESSION["data"] = $text;
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			$_SESSION["data"] = $replyToken;
+			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
@@ -35,7 +29,7 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
-			$_SESSION["data"] = $post;
+			
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -51,7 +45,6 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo $Chat;
-echo $_SESSION["data"];
+
 echo "\n";
 echo "OK";
